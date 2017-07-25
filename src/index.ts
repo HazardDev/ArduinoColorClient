11aaa53b324e850212fe2f97d4667f5e59cdfaa2
Jonathan Alexander <notdisliked@gmail.com>
2017-07-24 23:02:02 -0700

    Add anarchy (random) pattern

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,10 @@ const config = require('./config.json').general;
 const client = new UDPClient(config.ipAddress, config.port);
 
 import { SolidPattern, SolidBluePattern, SolidGreenPattern, SolidRedPattern } from "./patterns/solid_colors";
+import { AnarchyPattern } from "./patterns/anarchy";
 
 client.queue(new SolidBluePattern().getPixels());
 client.queue(new SolidGreenPattern().getPixels());
 client.queue(new SolidRedPattern().getPixels());
 client.queue(new SolidPattern(new Color(255, 255, 255)).getPixels());
+client.queue(new AnarchyPattern().getPixels());
