2cfb7ba2f27f24be3e0ea15ad6ab8a6037e25df0
Logan Saso <logan.saso2016@gmail.com>
2017-07-25 00:46:53 -0700

    Merged notdisliked-master into master

--- App/src/index.ts
+++ App/src/index.ts
@@ -6,10 +6,11 @@ const config = require('./config.json').general;
 const client = new UDPClient(config.ipAddress, config.port);
 
 import { SolidPattern, SolidBluePattern, SolidGreenPattern, SolidRedPattern } from "./patterns/solid_colors";
+import { AnarchyPattern } from "./patterns/anarchy";
 
-//client.queue(new SolidBluePattern().getPixels());
-const pixelList = new SolidBluePattern().getPixels();
-client.queue(pixelList);
-// client.queue(new SolidGreenPattern().getPixels());
-//client.queue(new SolidRedPattern().getPixels());
-// client.queue(new SolidPattern(new Color(255, 255, 255)).getPixels());
+
+client.queue(new SolidBluePattern().getPixels());
+client.queue(new SolidGreenPattern().getPixels());
+client.queue(new SolidRedPattern().getPixels());
+client.queue(new SolidPattern(new Color(255, 255, 255)).getPixels());
+client.queue(new AnarchyPattern().getPixels());
